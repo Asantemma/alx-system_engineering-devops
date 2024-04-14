b7d436c8f506c0b92d582acd10e343b37ba1ef8a
Emmanuel Asante <asantemma7@gmail.com>
2024-04-14 15:45:31 +0000

    Added API advanced files

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A Script that prints out top ten hottest posts
+of a subreddit"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """ function that queries the Reddit API and prints the titles of the first 10 
+    hot posts listed for a given subreddit"""
+
+    apiUrl = "https://reddit.com/r/{}/hot.json".format(subreddit)
+    userAgent = "Mozilla/5.0"
+    limits = 10
+
+    response = requests.get(
+        apiUrl, headers={"user-agent": userAgent}, params={"limit": limits})
+    if not response:
+        print('None')
+        return
+    response = response.json()
+    list_obj = response['data']['children']
+    for obj in list_obj:
+        print(obj['data']['title'])
+    return
